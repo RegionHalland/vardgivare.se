d49ef0480a24093ea087329e00bea178897ddb2a
Hydén Roland RGS IT APP <rhn014@lthalland.se>
2018-11-29 16:20:40 +0100

    tagit bort GA

--- web/app/themes/regionhalland/resources/views/layouts/app.blade.php
+++ web/app/themes/regionhalland/resources/views/layouts/app.blade.php
@@ -3,15 +3,6 @@
 	@include('partials.head')
 	<body @php(body_class())>
 		
-		{{--
-		<!--	
-		 Google Tag Manager (noscript)
-		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WJTVNM6"
-		height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-		End Google Tag Manager (noscript)
-		-->
-		--}}	
-
 		@include('partials.jump-to-content')
 		@include('partials.cookie-notice')
 		@include('partials.site-notices')
--- web/app/themes/regionhalland/resources/views/partials/head.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/head.blade.php
@@ -15,19 +15,8 @@
 	<meta name="msapplication-config" content="@asset('images/favicons/browserconfig.xml')" />
 	<meta name="theme-color" content="#ffffff" />
 
-	<!-- Google Tag Manager
-	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': 
-	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-	})(window,document,'script','dataLayer','GTM-WJTVNM6');</script>
-	End Google Tag Manager -->
-	
-	<!-- Browsealoud || Talande Webb 
-	<script type="text/javascript">var _baTheme=0, _baMode='Aktivera Talande Webb', _baUseCookies=true, _baHideOnLoad=true;</script>
-	Browsealoud || Talande Webb -->
-	
-  	<link rel='stylesheet' href='{!! env('WP_HOME') !!}/include/style/main.min.css' type='text/css' media='all' />
+	<link rel='stylesheet' href='{!! env('WP_HOME') !!}/include/style/main.min.css' type='text/css' media='all' />
   	<link rel='stylesheet' href='{!! env('WP_HOME') !!}/include/style/main.css' type='text/css' media='all' />  	
   	<link rel='stylesheet' href='{!! env('WP_HOME') !!}/include/style/std-style.css' type='text/css' media='all' />
+  	
 </head>
