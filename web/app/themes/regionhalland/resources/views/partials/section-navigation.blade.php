14c631d5c5a02bef146c284befdb795e4f3effbc
Hydén Roland RGS IT APP <rhn014@lthalland.se>
2018-12-10 14:15:56 +0100

    Tagit bort shortcodes via function

--- web/app/themes/regionhalland/resources/views/partials/section-navigation.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/section-navigation.blade.php
@@ -5,7 +5,6 @@
 <ul class="flex flex-wrap list-reset items-stretch -mx-4" aria-labelledby="Länklista">
 	@php($page_children = get_region_halland_page_children())
 	@foreach($page_children as $index => $page)
-		@php($arrPostcontent = explode("[styrdadokument", $page->post_content))
 		<li class="w-full sm:w-6/12 lg:w-6/12 px-4 mb-8">
 			<a href="{{ $page->url }}" class="text-blue-dark hover:bg-yellow-light focus:bg-yellow-light inline-block">
 				<h2 class="mb-2 text-xl md:text-2xl">{{ $page->post_title }}</h2>
@@ -14,7 +13,7 @@
 				@if(has_excerpt($page->ID)) 
 					{{ $page->acf_excerpt }}
 				@else
-					{{ html_entity_decode(wp_trim_words($arrPostcontent[0], 10, '...'))  }}
+					{{ html_entity_decode(wp_trim_words(region_halland_remove_shortcode($page->post_content), 10, '...'))  }}
 				@endif
 			</p>
 		</li>
