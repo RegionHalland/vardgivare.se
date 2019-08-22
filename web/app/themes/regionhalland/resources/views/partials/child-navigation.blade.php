8ca19ef941220bc082a3d0a214f7499bcc7967ee
rhn014 <Roland.Hyden@RegionHalland.se>
2019-08-22 11:16:36 +0200

    lagt till child-navigation

---
+++ web/app/themes/regionhalland/resources/views/partials/child-navigation.blade.php
@@ -0,0 +1,15 @@
+<nav aria-label="Undersidor" style="background-color: #F4F4F4;">
+    <ul class="flex flex-wrap px3 pt3 center" aria-label="Undersidor" style="max-width: 1440px;">
+		@php($page_children = get_region_halland_page_children())	
+			@foreach($page_children as $page)
+            <li class="rh-navigation-card left-align col-12 sm-col-6 md-col-4 lg-col-3 pr2" style="position:relative">
+                <div class="rh-navigation-card-title">
+                    <span class="rh-navigation-card-title-icon"></span>
+                    <strong><a href="{{ $page->url }}" class="h3" style="color:black; text-decoration: none;">
+                        {{ $page->post_title }}
+                    </a></strong>
+                </div>
+            </li>
+        @endforeach
+    </ul>
+</nav>
