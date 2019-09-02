15e94c051c8acc6549e8a87e24c82feb14800d27
Khoi Le <31632421+nguyenkhois@users.noreply.github.com>
2019-09-02 14:34:41 +0200

    Added the structure

--- web/app/themes/regionhalland/resources/views/partials/footer.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer id="footer-top-placeholder" style="background-color: #C3DCC1; line-height: 1.4;">
-    <div class="clearfix center px3 pb3" style="max-width: 1440px;">
-
+    {{-- <div class="clearfix center px3 pb3" style="max-width: 1440px;"> --}}
+    <div class="clearfix rh-container--auto rh-container-px">
         <div class="left-align col col-12 sm-col-12 md-col-3 lg-col-3">
             <p class="h2 pt3">Kontakta Region Halland</p>
             <ul>
