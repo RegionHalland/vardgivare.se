3436f9d00952de9f7c11af438e81e749eb540029
Hydén Roland <admrhn014@lthalland.se>
2019-09-02 15:50:38 +0200

    local build 20190902

--- web/app/themes/regionhalland/resources/views/partials/head.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/head.blade.php
@@ -34,7 +34,9 @@
 	</style> --}}
     {{-- <script type='text/javascript' src='{!! env('WP_HOME') !!}/include/scripts/jquery.3.3.1.min.js?ver=3.1.1'></script>--}}
    
-    <link rel="stylesheet" type="text/css" href="http://styleguide.local/vardgivare/vardgivare.css">
+    {{-- <link rel="stylesheet" type="text/css" href="http://styleguide.local/vardgivare/vardgivare.css"> --}}
+    <link rel="stylesheet" type="text/css" href="{!! env('WP_HOME') !!}/styleguide_cdn/vardgivare/vardgivare.css">
+
     <link rel="stylesheet" type="text/css" href="{!! env('WP_HOME') !!}/include/style/development.css">
 
     <link rel="stylesheet" type="text/css" href="{!! env('WP_HOME') !!}/_dev/css/development.css">
