ac466c090266b69f9981cb71a778192c94ada255
Khoi Le <31632421+nguyenkhois@users.noreply.github.com>
2019-09-03 12:31:09 +0200

    Fixed columns for navigation block

--- web/app/themes/regionhalland/resources/views/partials/front-page-news.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/front-page-news.blade.php
@@ -1,11 +1,11 @@
-@php($myNews = get_region_halland_news_archive_taxonomi_category_items(4))
+@php($myNews = get_region_halland_news_archive_taxonomi_category_items(3))
 @foreach($myNews as $news)
     <article class="rh-search-elements">
         <h2 class="h3"><a href="{{ $news->permalink }}" title="" class="rh-search-elements-title" style="color:black">
             {{ $news->post_title }}
         </a></h2>
         <span class="rh-search-elements-inline">Publicerad: <time itemprop="datePublished" datetime="{{ $news->date }}">{{ $news->date }}</time></span>
-        <p class="rh-search-elements-description">{{ html_entity_decode(wp_trim_words($news->post_content, 60, '...')) }}</p> 
+        <p class="rh-search-elements-description">{{ html_entity_decode(wp_trim_words($news->post_content, 40, '...')) }}</p> 
     </article>
 @endforeach
 <span><a href="{!! env('WP_HOME') !!}/nyheter/">Visa alla nyheter</a></span>
