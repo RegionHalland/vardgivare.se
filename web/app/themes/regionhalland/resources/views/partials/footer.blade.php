30638e38122298d4a7c372d1e467941b3c26154e
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-09-15 13:06:41 +0300

    Övriga webbplatser läses nu upp som en lista av skärmläsare istället för grupp

--- web/app/themes/regionhalland/resources/views/partials/footer.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/footer.blade.php
@@ -1,3 +1,5 @@
+@php($id2 = uniqid())
+
 <div class="rh-vg__styling-dark">
     <div class="clearfix rh-footer-group rh-container--auto rh-footer-group__container-p">
         <div class="row row-eq-height">
@@ -32,10 +34,10 @@
             <div class="col col-12 md-col-4 rh-footer-group__item">
                 <div>
                     <div class="rh-footer-group__item-header">
-                        <p class="rh-footer-group__item-header-title">Övriga webbplatser</p>
+                        <p id="$id2" class="rh-footer-group__item-header-title">Övriga webbplatser</p>
                     </div>
 
-                    <ul class="rh-footer-group__item-body" aria-label="Övriga webbplatser" role="group">
+                    <ul class="rh-footer-group__item-body" aria-labelledby="$id2" role="list">
                         <li><a href="#" class="rh-link--navigation rh-footer-group__item-body-link">Regionhalland.se</a>
                         </li>
                         <li><a href="#"
