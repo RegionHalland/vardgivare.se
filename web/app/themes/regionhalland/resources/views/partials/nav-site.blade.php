7cddf24fde7f95b1fc872dc220a56853016a505c
Khoi Le <31632421+nguyenkhois@users.noreply.github.com>
2019-09-17 11:45:02 +0200

    Using data from db

--- web/app/themes/regionhalland/resources/views/partials/nav-site.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/nav-site.blade.php
@@ -77,6 +77,9 @@
 
 
 <div class="rh-menu__overlay rh-dp--none"></div>
+
+@if(function_exists('get_region_halland_tree_all_levels_vardgivare'))
+@php($myTree = get_region_halland_tree_all_levels_vardgivare())
 <div id="rh-menu-body" class="rh-menu__body rh-pos--fixed">
 
     <div class="rh-menu__top-bar">
@@ -88,131 +91,117 @@
 
     <div class="rh-menu__offset-top"></div>
 
-
     {{-- Item - Level 1 - Root --}}
+    @foreach ($myTree as $tree1)
+    @php ($level1ChildrenCount = count($tree1['children']))
     <div>
-        <div class="rh-menu__item rh-menu__item-level-1 rh-pl-1">
-            <a href="#" class="rh-link--navigation rh-menu__link-level-1">
-                Behandlingsstöd
+        <div class="rh-menu__item  rh-menu__item-level-1 rh-pl-1">
+            <a href="{{ $tree1['page_url'] }}" class="rh-link--navigation rh-menu__link-level-1">
+                {{ $tree1['post_title'] }}
             </a>
 
-            <div id="1" class="rh-round-button rh-round-button--small rh-menu__item-button rh-menu__item-button-parent">
+            @if($level1ChildrenCount > 0)
+            <div id="{{ $tree1['ID'] }}" class="rh-round-button rh-round-button--small rh-menu__item-button rh-menu__item-button-parent">
                 <span class="icon-plus"></span>
             </div>
+            @endif
         </div>
-
 
         {{-- Sub item container - Level 2 --}}
-        <div id="sub1" class="rh-menu__item-sub-container rh-dp--none">
+        @if($level1ChildrenCount > 0)
+        <div id="sub{{ $tree1['ID'] }}" class="rh-menu__item-sub-container rh-dp--none">
+            @foreach ($tree1['children'] as $tree2)
+            @php ($level2ChildrenCount = count($tree2['children']))
             <div class="rh-menu__item rh-menu__item-sub-item rh-pl-1">
-                <a href="#" class="rh-link--navigation rh-menu__link">
-                    Diagnos- och åtgärdskodning primärvård
-                </a>
-            </div>
-
-            <div class="rh-menu__item rh-menu__item-sub-item rh-pl-1">
-                <a href="#" class="rh-link--navigation rh-menu__link">
-                    Smittskydd
-                </a>
-            </div>
-
-            <div class="rh-menu__item rh-menu__item-sub-item rh-pl-1">
-                <a href="#" class="rh-link--navigation rh-menu__link">
-                    Läkemedel
+                <a href="{{ $tree2['page_url'] }}" class="rh-link--navigation rh-menu__link">
+                    {{ $tree2['post_title'] }}
                 </a>
 
-                <div id="13"
-                    class="rh-round-button rh-round-button--small rh-menu__item-button rh-menu__item-button-sub-item--active">
+                @if($level2ChildrenCount > 0)
+                <div id="{{ $tree2['ID'] }}" class="rh-round-button rh-round-button--small rh-menu__item-button rh-menu__item-button-sub-item--active">
                     <span class="icon-plus"></span>
                 </div>
+                @endif
             </div>
 
-            {{-- Level 3 --}}
-            <div id="sub13" class="rh-menu__item-sub-container rh-menu__item-sub-container-level-3 rh-dp--none">
+            {{-- Sub item container - Level 3 --}}
+            @if($level2ChildrenCount > 0)
+            <div id="sub{{ $tree2['ID'] }}" class="rh-menu__item-sub-container rh-menu__item-sub-container-level-3 rh-dp--none">
+                @foreach ($tree2['children'] as $tree3)
+                @php ($level3ChildrenCount = count($tree3['children']))
                 <div class="rh-menu__item rh-menu__item-sub-item rh-pl-2">
-                    <a href="#" class="rh-link--navigation rh-menu__link">
-                        Beställa läkemedel
-                    </a>
-                </div>
-
-                <div class="rh-menu__item rh-menu__item-sub-item rh-pl-2">
-                    <a href="#" class="rh-link--navigation rh-menu__link">
-                        Läkemedelsgenomgångar
+                    <a href="{{ $tree3['page_url'] }}" class="rh-link--navigation rh-menu__link">
+                        {{ $tree3['post_title'] }}
                     </a>
 
-                    <div id="132"
-                        class="rh-round-button rh-round-button--small rh-menu__item-button rh-menu__item-button-sub-item--active">
+                    @if($level3ChildrenCount > 0)
+                    <div id="{{ $tree3['ID'] }}" class="rh-round-button rh-round-button--small rh-menu__item-button rh-menu__item-button-sub-item--active">
                         <span class="icon-plus"></span>
                     </div>
+                    @endif
                 </div>
 
-                {{-- Level 4 --}}
-                <div id="sub132" class="rh-menu__item-sub-container rh-menu__item-sub-container-level-4 rh-dp--none">
+                {{-- Sub item container - Level 4 --}}
+                @if($level3ChildrenCount > 0)
+                <div id="sub{{ $tree3['ID'] }}" class="rh-menu__item-sub-container rh-menu__item-sub-container-level-4 rh-dp--none">
+                    @foreach ($tree3['children'] as $tree4)
+                    @php ($level4ChildrenCount = count($tree4['children']))
                     <div class="rh-menu__item rh-menu__item-sub-item rh-pl-3">
-                        <a href="#" class="rh-link--navigation rh-menu__link">
-                            Äldre och läkemedel
-                        </a>
-                    </div>
-
-                    <div class="rh-menu__item rh-menu__item-sub-item rh-pl-3">
-                        <a href="#" class="rh-link--navigation rh-menu__link">
-                            Verktyg vid läkemedelsgenomgångar
+                        <a href="{{ $tree4['page_url'] }}" class="rh-link--navigation rh-menu__link">
+                            {{ $tree4['post_title'] }}
                         </a>
 
-                        <div id="1322"
-                            class="rh-round-button rh-round-button--small rh-menu__item-button rh-menu__item-button-sub-item--active">
+                        @if($level4ChildrenCount > 0)
+                        <div id="{{ $tree4['ID'] }}" class="rh-round-button rh-round-button--small rh-menu__item-button rh-menu__item-button-sub-item--active">
                             <span class="icon-plus"></span>
                         </div>
+                        @endif
                     </div>
 
-                    {{-- Level 5 --}}
-                    <div id="sub1322" class="rh-menu__item-sub-container rh-menu__item-sub-container-level-5 rh-dp--none">
+                    {{-- Sub item container - Level 5 --}}
+                    @if($level4ChildrenCount > 0)
+                    <div id="sub{{ $tree4['ID'] }}" class="rh-menu__item-sub-container rh-menu__item-sub-container-level-5 rh-dp--none">
+                        @foreach ($tree4['children'] as $tree5)
+                        @php ($level5ChildrenCount = count($tree5['children']))
                         <div class="rh-menu__item rh-menu__item-sub-item rh-pl-4">
-                            <a href="#" class="rh-link--navigation rh-menu__link">
-                                Arbetsgång vid läkemedelsgenomgång
+                            <a href="{{ $tree5['page_url'] }}" class="rh-link--navigation rh-menu__link">
+                                {{ $tree5['post_title'] }}
                             </a>
-                        </div>
-                        <div class="rh-menu__item rh-menu__item-sub-item rh-pl-4">
-                            <a href="#" class="rh-link--navigation rh-menu__link">
-                                Checklista vid läkemedelsgenomgång
-                            </a>
-                        </div>
-                    </div>
 
-                    <div class="rh-menu__item rh-menu__item-sub-item rh-pl-3">
-                        <a href="#" class="rh-link--navigation rh-menu__link rh-menu__link">
-                            Patientkost
-                        </a>
-                    </div>
+                            @if($level5ChildrenCount > 0)
+                            <div id="{{ $tree5['ID'] }}" class="rh-round-button rh-round-button--small rh-menu__item-button rh-menu__item-button-sub-item--active">
+                                <span class="icon-plus"></span>
+                            </div>
+                            @endif
+                        </div>
 
-                    <div class="rh-menu__item rh-menu__item-sub-item rh-pl-3">
-                        <a href="#" class="rh-link--navigation rh-menu__link rh-menu__link">
-                            Patientinformation
-                        </a>
+                        {{-- Sub item container - Level 6 --}}
+                        @if($level5ChildrenCount > 0)
+                        <div id="sub{{ $tree5['ID'] }}" class="rh-menu__item-sub-container rh-menu__item-sub-container-level-5 rh-dp--none">
+                            @foreach ($tree5['children'] as $tree6)
+                            @php ($level6ChildrenCount = count($tree6['children']))
+                            <div class="rh-menu__item rh-menu__item-sub-item rh-pl-5">
+                                <a href="{{ $tree6['page_url'] }}" class="rh-link--navigation rh-menu__link">
+                                    {{ $tree6['post_title'] }}
+                                </a>
+                            </div>
+                            @endforeach
+                        </div>
+                        @endif {{-- End fo level 6 --}}
+                        @endforeach
                     </div>
+                    @endif {{-- End fo level 5 --}}
+                    @endforeach
                 </div>
+                @endif {{-- End fo level 4 --}}
+                @endforeach
             </div>
-
+            @endif {{-- End of level 3 --}}
+            @endforeach
         </div>
+        @endif {{-- End of level 2 --}}
     </div>
-
-    {{-- Item - Level 1 - Root --}}
-    <div>
-        <div class="rh-menu__item  rh-menu__item-level-1 rh-pl-1">
-            <a href="#" class="rh-link--navigation rh-menu__link-level-1">
-                Kompetens och utveckling
-            </a>
-        </div>
-    </div>
-
-    {{-- Item - Level 1 - Root --}}
-    <div>
-        <div class="rh-menu__item  rh-menu__item-level-1 rh-pl-1">
-            <a href="#" class="rh-link--navigation rh-menu__link-level-1">
-                Medicinska områden
-            </a>
-        </div>
-    </div>
-
+    @endforeach
     <div class="rh-menu__offset-bottom"></div>
 </div>
+@endif
