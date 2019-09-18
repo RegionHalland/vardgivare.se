8cdb227ac53cad3c5db01f6b8190ffd1205db526
rhn014 <Roland.Hyden@RegionHalland.se>
2019-09-18 10:24:37 +0200

    Fixat cookie-notice close

--- web/app/themes/regionhalland/resources/views/partials/cookie-notice.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/cookie-notice.blade.php
@@ -1,7 +1,7 @@
 @php($checkCookieNotice = check_region_halland_cookie_notice())
 @if ($checkCookieNotice == false)
 @php($cookie_notice = get_region_halland_cookie_notice())
-<div class="rh-vg__styling-light-1">
+<div id="cookie-notice" class="rh-vg__styling-light-1">
     <div class="rh-cookie rh-container--auto rh-container-px">
         <span class="rh-cookie-icon"></span>
         <p class="rh-cookie-description">
--- web/include/scripts/application.js
+++ web/include/scripts/application.js
@@ -51,6 +51,29 @@ $(function () {
         }
     });
 
+    // ****************************
+    // *** Cookie notice accept ***
+    // ****************************    
+    $("#cookie-consent").on( "click", function() {
+        // set cookie with vanilla javascript function
+        setCookie('cookie_notice_accepted','1',365);
+        // Hide div with cookie notice text + button
+        $("#cookie-notice").hide();
+    });
+
+
+    // **************************************
+    // *** Javascript set cookie function ***
+    // **************************************
+    function setCookie(name,value,days) {
+        var expires = "";
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days*24*60*60*1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    }
 
 
     // *****************************
