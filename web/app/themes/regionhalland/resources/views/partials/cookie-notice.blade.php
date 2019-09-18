35d68c0cb566db9663a2be4295a1d34322e6fe14
Khoi Le <31632421+nguyenkhois@users.noreply.github.com>
2019-09-18 09:34:22 +0200

    Completed Cookie notice message

--- web/app/themes/regionhalland/resources/views/partials/cookie-notice.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/cookie-notice.blade.php
@@ -5,13 +5,11 @@
     <div class="rh-cookie rh-container--auto rh-container-px">
         <span class="rh-cookie-icon"></span>
         <p class="rh-cookie-description">
-            {{-- {!! $cookie_notice['message'] !!} --}}
-            På den här webbplatsen använder vi cookies (kakor) för att webbplatsen ska fungera på ett bra sätt för dig. Genom att klicka vidare eller på “Jag förstår” godkänner du att vi använder cookies.
+            {!! $cookie_notice['message'] !!}
         </p>
         <button id="cookie-consent" class="rh-button rh-button--primary rh-cookie-button"
             aria-label="Acceptera cookies och dölj meddelandet">
-            Jag förstår
-            {{-- {!! $cookie_notice['button'] !!} --}}
+            {!! $cookie_notice['button'] !!}
         </button>
     </div>
 </div>
