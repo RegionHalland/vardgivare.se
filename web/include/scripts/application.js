ea4840fe42548a5e031f586ca263e2cee552fe93
Khoi Le <31632421+nguyenkhois@users.noreply.github.com>
2019-09-18 10:58:48 +0200

    Improved main menu button position

--- web/include/scripts/application.js
+++ web/include/scripts/application.js
@@ -93,31 +93,7 @@ $(function () {
         }, 10));
     }
 
-    // ****************************
-    // *** Cookie notice accept ***
-    // ****************************
-    $("#cookie-consent").on("click", function () {
-        // set cookie with vanilla javascript function
-        setCookie('cookie_notice_accepted', '1', 365);
-        // Hide div with cookie notice text + button
-        $(".rh-cookie").hide();
-    });
-
 });
-
-
-// **************************************
-// *** Javascript set cookie function ***
-// **************************************
-function setCookie(name, value, days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
 
 // ************************************
 // *** Javascript debounce function ***
