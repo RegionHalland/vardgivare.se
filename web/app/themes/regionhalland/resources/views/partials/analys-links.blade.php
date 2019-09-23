945a52303efd50686e1d930bcb0484ff315b2612
rhn014 <Roland.Hyden@RegionHalland.se>
2019-09-23 09:48:18 +0200

    Justerat länkar analysförteckningen

--- web/app/themes/regionhalland/resources/views/partials/analys-links.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/analys-links.blade.php
@@ -2,22 +2,22 @@
 <div class="mb2">
     <ul>
         <li class="content-nav__item" >
-            <a class="content-nav__item-link" href="http://vardgivare.regionhalland.se/app/uploads/2019/01/Prislista-Klinisk-mikrobiologi.pdf" target="_blank">
+            <a class="content-nav__item-link" href="https://vardgivare.regionhalland.se/app/uploads/2019/07/Prislista-Klinisk-mikrobiologi.pdf" target="_blank">
                 Prislista Klinisk mikrobiologi 2019 
             </a>
         </li>
         <li class="content-nav__item" >
-            <a class="content-nav__item-link" href="http://vardgivare.regionhalland.se/app/uploads/2018/07/Svarstider-Klinisk-mikrobiologi.pdf" target="_blank">
+            <a class="content-nav__item-link" href="https://vardgivare.regionhalland.se/app/uploads/2018/07/Svarstider-Klinisk-mikrobiologi.pdf" target="_blank">
                 Svarstider Klinisk mikrobiologi 
             </a>
         </li>
         <li class="content-nav__item" >
-            <a class="content-nav__item-link" href="http://vardgivare.regionhalland.se/app/uploads/2018/07/Remiss-mikrobiologen.pdf" target="_blank">
+            <a class="content-nav__item-link" href="https://vardgivare.regionhalland.se/app/uploads/2019/03/Klinisk-mikrobiologi-remiss.pdf" target="_blank">
                 Remiss Klinisk mikrobiologi 
             </a>
         </li>
         <li class="content-nav__item" >
-            <a class="content-nav__item-link" href="http://vardgivare.regionhalland.se/app/uploads/2019/03/Så-här-skriver-du-en-bra-remiss-till-mikrobiologen.pdf" target="_blank">
+            <a class="content-nav__item-link" href="https://vardgivare.regionhalland.se/app/uploads/2019/03/Så-här-skriver-du-en-bra-remiss-till-mikrobiologen.pdf" target="_blank">
                 Lathund – skriv bra remiss
             </a>
         </li>
@@ -27,27 +27,27 @@
 <div class="mb2">
     <ul>
         <li class="content-nav__item" >
-            <a class="content-nav__item-link" href="http://vardgivare.regionhalland.se/behandlingsstod/laboratoriemedicin/analysforteckning/forklaringar-analysforteckning-klinisk-kemi/" target="_blank">
+            <a class="content-nav__item-link" href="https://vardgivare.regionhalland.se/behandlingsstod/laboratoriemedicin/analysforteckning/forklaringar-analysforteckning-klinisk-kemi/" target="_blank">
                 Förklaringar analysförteckning 
             </a>
         </li>
         <li class="content-nav__item" >
-            <a class="content-nav__item-link" href="http://vardgivare.regionhalland.se/behandlingsstod/laboratoriemedicin/klinisk-kemi/provtagning-och-provhantering/" target="_blank">
+            <a class="content-nav__item-link" href="https://vardgivare.regionhalland.se/behandlingsstod/laboratoriemedicin/klinisk-kemi/provtagning-och-provhantering/" target="_blank">
                 Provtagning och provhantering
             </a>
         </li>
         <li class="content-nav__item" >
-            <a class="content-nav__item-link" href="http://vardgivare.regionhalland.se/behandlingsstod/laboratoriemedicin/klinisk-kemi/remisser-klinisk-kemi/" target="_blank">
+            <a class="content-nav__item-link" href="https://vardgivare.regionhalland.se/behandlingsstod/laboratoriemedicin/klinisk-kemi/remisser-klinisk-kemi/" target="_blank">
                 Remisser Klinisk kemi
             </a>
         </li>
         <li class="content-nav__item" >
-            <a class="content-nav__item-link" href="http://vardgivare.regionhalland.se/app/uploads/2018/07/VAS-provkoder-KEM-och-BAKT-5402-3.pdf" target="_blank">
+            <a class="content-nav__item-link" href="https://vardgivare.regionhalland.se/app/uploads/2018/07/VAS-provkoder-KEM-och-BAKT-5402-3.pdf" target="_blank">
                 VAS-provkoder KEM och BAKT
             </a>
         </li>
         <li class="content-nav__item" >
-            <a class="content-nav__item-link" href="http://vardgivare.regionhalland.se/app/uploads/2019/04/Prislista-Klinisk-kemi-20195318-14.pdf" target="_blank">
+            <a class="content-nav__item-link" href="https://vardgivare.regionhalland.se/app/uploads/2019/04/Prislista-Klinisk-kemi-20195318-14.pdf" target="_blank">
                 Prislista Klinisk kemi 2019
             </a>
         </li>
