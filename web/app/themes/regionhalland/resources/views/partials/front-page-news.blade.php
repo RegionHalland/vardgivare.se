4eca97273b259c248ab261dfaaec235f942fe2ab
Khoi Le <31632421+nguyenkhois@users.noreply.github.com>
2019-10-06 22:21:43 +0200

    Created article-block and article-group

--- web/app/themes/regionhalland/resources/views/partials/front-page-news.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/front-page-news.blade.php
@@ -2,25 +2,25 @@
 
 @php($myNews = get_region_halland_news_archive_taxonomi_category_items(3))
 <div class="mt3 rh-dp-from-lg"></div>
-<article class="rh-vg__home-news">
-    <div class="rh-vg__home-news__header">
-        <h2 id="$id" class="rh-vg__home-news__header-title">Nyheter</h2>
+<article class="rh-article-group">
+    <div class="rh-article-group__header">
+        <h2 id="$id" class="rh-article-group__header-title">Nyheter</h2>
     </div>
 
     <ul aria-labelledby="$id">
     @foreach($myNews as $news)
-    <li class="rh-vg__home-news__item">
-        <div class="rh-vg__home-news__item-publish">
-            <span class="rh-vg__home-news__item-publish-icon"></span>
-            <p class="rh-vg__home-news__item-publish-info">Publicerad: <time
+    <li class="rh-article-block">
+        <div class="rh-article-block__publish-date">
+            <span class="rh-article-block__publish-date-icon"></span>
+            <p class="rh-article-block__publish-date-info">Publicerad: <time
                 datetime="{{ $news->date }}">{{ $news->date }}</time></p>
         </div>
 
         <div>
-            <h3 class="rh-vg__home-news__item-title">
-                <a class="rh-link--navigation rh-vg__home-news__item-title-link" href="{{ $news->url }}" title="">{{ $news->post_title }}</a>
+            <h3 class="rh-article-block__title">
+                <a class="rh-link--navigation rh-article-block__title-link" href="{{ $news->url }}" title="">{{ $news->post_title }}</a>
             </h3>
-            <p class="rh-vg__home-news__item-description">
+            <p class="rh-article-block__description">
                 {{ html_entity_decode(wp_trim_words($news->post_content, 20, '...')) }}
             </p>
         </div>
@@ -29,7 +29,7 @@
     </ul>
 </article>
 
-<div class="rh-vg__home-news__view-more">
+<div class="rh-article-group__view-more">
     <a class="rh-button rh-button--icon" style="text-decoration: none;" aria-label="Visa alla nyheter" role="button"
         href="{!! env('WP_HOME') !!}/nyheter/">
         Visa alla nyheter
--- web/app/themes/regionhalland/resources/views/partials/news-section-landing.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/news-section-landing.blade.php
@@ -2,25 +2,25 @@
 @php($newsitems = get_region_halland_page_news_taxonomi_category(1))
 @if($newsitems)
 <div class="col col-12 md-col-6 lg-col-12">
-    <article class="rh-news-block">
-        <div class="rh-news-block__header">
-            <h2 id="$id" class="rh-news-block__header-title">Nyheter</h2>
+    <article class="rh-article-group">
+        <div class="rh-article-group__header">
+            <h2 id="$id" class="rh-article-group__header-title">Nyheter</h2>
         </div>
 
         <ul aria-labelledby="$id">
         @foreach($newsitems as $item)
-        <li class="rh-news-block__item">
-            <div class="rh-news-block__item-publish">
-                <span class="rh-news-block__item-publish-icon"></span>
-                <p class="rh-news-block__item-publish-info">Publicerad: <time
+        <li class="rh-article-block">
+            <div class="rh-article-block__publish-date">
+                <span class="rh-article-block__publish-date-icon"></span>
+                <p class="rh-article-block__publish-date-info">Publicerad: <time
                     datetime="{{ $item['date'] }}">{{ $item['date'] }}</time></p>
             </div>
 
             <div>
-                <h3 class="rh-news-block__item-title">
-                    <a class="rh-link--navigation rh-news-block__item-title-link" href="{{ $item['permalink'] }}" title="">{{ $item['title'] }}</a>
+                <h3 class="rh-article-block__title">
+                    <a class="rh-link--navigation rh-article-block__title-link" href="{{ $item['permalink'] }}" title="">{{ $item['title'] }}</a>
                 </h3>
-                <p class="rh-news-block__item-description">
+                <p class="rh-article-block__description">
                     {{ wp_trim_words(region_halland_remove_shortcode($item['content']), 20, '...') }}
                 </p>
             </div>
@@ -29,7 +29,7 @@
         </ul>
     </article>
 
-    <div class="rh-news-block__view-more">
+    <div class="rh-article-group__view-more">
         <a class="rh-button rh-button--icon" style="text-decoration: none;" aria-label="Visa alla nyheter" role="button"
             href="{!! env('WP_HOME') !!}/nyheter/">
             Se alla nyheter inom {!! get_the_title() !!}
