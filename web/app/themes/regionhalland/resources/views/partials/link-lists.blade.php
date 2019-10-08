13f1c59e6194492bf02e354d52e02b0cb7e555ff
RolandHyden <Roland.Hyden@RegionHalland.se>
2019-10-08 11:22:21 +0200

    Uppdaterad composer

--- web/app/themes/regionhalland/resources/views/partials/link-lists.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/link-lists.blade.php
@@ -1,7 +1,7 @@
 @php($myLinks = get_region_halland_acf_page_link_lists_items())
 @if($myLinks)
 	@foreach ($myLinks as $link)
-		<h3>{{ $link['link_title'] }}</h3>
+		<h2 id="{{ $link['link_slug'] }}">{{ $link['link_title'] }}</h2>
 		@foreach ($link['link_list'] as $list)
 			<a href="{{$list['link_url']}}" target="{{$list['link_target']}}">{{$list['link_title']}}</a><br><br>
 		@endforeach
